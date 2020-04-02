0aa44797f9a17e2d8f76295e4d17014ad6b3c93f
Butch Masters <butch@carbonrelay.com>
2020-04-01 20:06:18 -0400

    Brew formula update for konjure version v0.2.1

--- Formula/konjure.rb
+++ Formula/konjure.rb
@@ -2,16 +2,16 @@
 class Konjure < Formula
   desc "Manifest appear!"
   homepage "https://github.com/carbonrelay/konjure/"
-  version "0.2.0"
+  version "0.2.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/carbonrelay/konjure/releases/download/v0.2.0/konjure-darwin-amd64.tar.gz"
-    sha256 "4c669ad83ca901ee663e1e177fa8bd0997c69a8b061fc673a2bdbddad5805e44"
+    url "https://github.com/carbonrelay/konjure/releases/download/v0.2.1/konjure-darwin-amd64.tar.gz"
+    sha256 "e1b23023d33f006fbd63ebfd9f325fe0ac51bc02adc3e5007af46a53efce86be"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/carbonrelay/konjure/releases/download/v0.2.0/konjure-linux-amd64.tar.gz"
-      sha256 "a9812e1a29d7dca2afc3606d860c5e2f5674da6c678ca8707f9378de547f69fa"
+      url "https://github.com/carbonrelay/konjure/releases/download/v0.2.1/konjure-linux-amd64.tar.gz"
+      sha256 "8bf2a82b389076d80a9bd5f379c330e5d74353ef8fac95f851dd26c26349b61c"
     end
   end
 
