28d3ab734120c924c2594a206d032148001ad9ed
Butch Masters <butch@carbonrelay.com>
2020-07-23 18:56:58 -0400

    Brew formula update for redskyctl version v1.6.1

--- Formula/redskyctl.rb
+++ Formula/redskyctl.rb
@@ -2,16 +2,16 @@
 class Redskyctl < Formula
   desc "Kubernetes Exploration"
   homepage "https://redskyops.dev/"
-  version "1.6.0"
+  version "1.6.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/redskyops/redskyops-controller/releases/download/v1.6.0/redskyctl-darwin-amd64.tar.gz"
-    sha256 "ef125f6c1040205588d7e09dc4e441eb292d27c23f434484ffe406feffc407d1"
+    url "https://github.com/redskyops/redskyops-controller/releases/download/v1.6.1/redskyctl-darwin-amd64.tar.gz"
+    sha256 "3ded17b58965e982f26aedfd19d140e0231045451955f115fa9b211871cde8c3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/redskyops/redskyops-controller/releases/download/v1.6.0/redskyctl-linux-amd64.tar.gz"
-      sha256 "d192514455edb2684f648c75b975956f97dc661d2220ad9d421466a466f30274"
+      url "https://github.com/redskyops/redskyops-controller/releases/download/v1.6.1/redskyctl-linux-amd64.tar.gz"
+      sha256 "2aa4431bcd29d8e5ae3266803c4a0b8b18427ef067cc4ea7278438b9a9d9cf87"
     end
   end
 
