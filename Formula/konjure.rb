c56b1bf8c3c1d5ec44b309be0f0c34483238d6a2
Butch Masters <butch@stormforge.io>
2021-02-23 10:34:31 -0500

    Brew formula update for konjure version v0.3.0-beta.3

--- Formula/konjure.rb
+++ Formula/konjure.rb
@@ -5,16 +5,16 @@
 class Konjure < Formula
   desc "Manifest appear!"
   homepage "https://github.com/thestormforge/konjure/"
-  version "0.3.0-beta.2"
+  version "0.3.0-beta.3"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/thestormforge/konjure/releases/download/v0.3.0-beta.2/konjure-darwin-amd64.tar.gz"
-    sha256 "684db310505f538add4874470efd2b562ed7dc5acf54d562f64630a2108b4444"
+    url "https://github.com/thestormforge/konjure/releases/download/v0.3.0-beta.3/konjure-darwin-amd64.tar.gz"
+    sha256 "83d38d77a6ddec5ff82e120a10f4abb470ee2b909db946d2dfebe2a19d1dd354"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/thestormforge/konjure/releases/download/v0.3.0-beta.2/konjure-linux-amd64.tar.gz"
-    sha256 "f0bb75d1626acad8685e1d7277d718dfb5307ebc0b01401bee0117a9f218dd3b"
+    url "https://github.com/thestormforge/konjure/releases/download/v0.3.0-beta.3/konjure-linux-amd64.tar.gz"
+    sha256 "5bcb7fe5bc7f278182a24a3cd5e41e9e80518519ea6598ea029d3a7cd425a964"
   end
 
   def install
