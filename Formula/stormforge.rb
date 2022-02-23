732e20fdaf6ac48de7e3f5b86e6a7d4e79c2fa3b
Butch Masters <butch@stormforge.io>
2022-02-22 19:51:33 -0500

    Brew formula update for stormforge version v2.0.8

--- Formula/stormforge.rb
+++ Formula/stormforge.rb
@@ -5,20 +5,20 @@
 class Stormforge < Formula
   desc "Release with Confidence"
   homepage "https://www.stormforge.io/"
-  version "2.0.7"
+  version "2.0.8"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/thestormforge/optimize-controller/releases/download/v2.0.7/stormforge-darwin-arm64.tar.gz"
-      sha256 "1f33b23fbfd958d2d106ccd685697a069d6d40cca143a266b52db4ea97540e5f"
+      url "https://github.com/thestormforge/optimize-controller/releases/download/v2.0.8/stormforge-darwin-arm64.tar.gz"
+      sha256 "b52d5d696182741f959b4b673cf8ca9e378a7ee7043266a38dcb0e42f06b3b32"
 
       def install
         bin.install "stormforge"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/thestormforge/optimize-controller/releases/download/v2.0.7/stormforge-darwin-amd64.tar.gz"
-      sha256 "51bded704e6440fbea53df9b3b3de32a293f3b764b542399a8cacf4872c29e64"
+      url "https://github.com/thestormforge/optimize-controller/releases/download/v2.0.8/stormforge-darwin-amd64.tar.gz"
+      sha256 "4d974ce377df24c6348935616daa5647ffb7bc92e6711233e2e554368c1573bf"
 
       def install
         bin.install "stormforge"
@@ -28,8 +28,8 @@ class Stormforge < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/thestormforge/optimize-controller/releases/download/v2.0.7/stormforge-linux-amd64.tar.gz"
-      sha256 "424f776d90eeda291de568459a65dd45899d7603399ff14016807627d427f011"
+      url "https://github.com/thestormforge/optimize-controller/releases/download/v2.0.8/stormforge-linux-amd64.tar.gz"
+      sha256 "e2c30a2d581d014b41371931a8202412d6cc55242082edfd469378d93ad8a294"
 
       def install
         bin.install "stormforge"
