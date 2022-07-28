3d85cf95893ab0a6100ce1012066a8dc3c39265d
Butch Masters <butch@stormforge.io>
2022-07-28 12:25:56 -0400

    Brew formula update for konjure version v0.3.14

--- Formula/konjure.rb
+++ Formula/konjure.rb
@@ -5,20 +5,20 @@
 class Konjure < Formula
   desc "Manifest appear!"
   homepage "https://github.com/thestormforge/konjure/"
-  version "0.3.13"
+  version "0.3.14"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/thestormforge/konjure/releases/download/v0.3.13/konjure-darwin-amd64.tar.gz"
-      sha256 "2b91ed6bfa90de30332a255cb1e620077520103588c9b2187716c178284b2bf3"
+      url "https://github.com/thestormforge/konjure/releases/download/v0.3.14/konjure-darwin-amd64.tar.gz"
+      sha256 "d865edac96ef4d416558f65310bf281228f2fbb3f34e59d650bdba9f4e8298b8"
 
       def install
         bin.install "konjure"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/thestormforge/konjure/releases/download/v0.3.13/konjure-darwin-arm64.tar.gz"
-      sha256 "57b94ef02ed0ba2b7a7afccf5a5f1596ae74c1f36e7d3daad7b26ca564848398"
+      url "https://github.com/thestormforge/konjure/releases/download/v0.3.14/konjure-darwin-arm64.tar.gz"
+      sha256 "cf0ee9e46e20c03d5b3ea5ebd3e2496daafc3ce59d0eb6a789701d2a4a20555e"
 
       def install
         bin.install "konjure"
@@ -28,8 +28,8 @@ class Konjure < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/thestormforge/konjure/releases/download/v0.3.13/konjure-linux-amd64.tar.gz"
-      sha256 "819e9dc21629844be7377c892d2d7957e3d2f090d919abbce655dcffba859aa8"
+      url "https://github.com/thestormforge/konjure/releases/download/v0.3.14/konjure-linux-amd64.tar.gz"
+      sha256 "166953f7203dfb5172771d43c113ced9dfd15b3dc350025625a82cce7889612b"
 
       def install
         bin.install "konjure"
